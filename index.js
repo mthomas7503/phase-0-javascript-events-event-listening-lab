fe75b415103b9a920dc8719042030b1395eb58ce
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-03-14 12:36:17 -0400

    commit tree 6a7ad70f20c12629fca4233274f7e6f39d8d7910

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-function addingEventListener() {
-}
+function addingEventListener() {const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+})}
